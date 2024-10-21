43eb7b10cd678888b6ca5b9b18d41571ac4d3bc9
rtf <rtf@fakeemail.real>
2024-10-21 19:00:27 +0100

    1 new package(s)

--- repo.py
+++ repo.py
@@ -1,4 +1,5 @@
 repo = {
     # 'pkgname':'url'
-    'catfact':'https://github.com/Redstone-The-Fox/catfact'
+    'catfact':'https://github.com/Redstone-The-Fox/catfact',
+    'fpm':'https://github.com/Redstone-The-Fox/fpm'
 }
