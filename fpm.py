38c8fdf9442441c789c5c64974c26bb18200daba
rtf <rtf@fakeemail.real>
2024-10-21 19:06:20 +0100

    change fpm.py

--- fpm.py
+++ fpm.py
@@ -9,10 +9,10 @@ except ModuleNotFoundError:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--install', help='installs a package')
-parser.add_argument('version')
+parser.add_argument('-v','--version', help='outputs version', action='store_true')
 args = parser.parse_args()
-if args.version == True:
-    prunt('FPM Version 0.1')
+if args.version:
+    exit('FPM Version 0.1')
 if str(args.install) in repo:
     os.system(f'git clone {repo[args.install]}')
 else:
